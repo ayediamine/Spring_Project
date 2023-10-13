f3b043dd0322e2f017e9a648f0bd29f3b80a8be3
ayediamine <amine.ayadi@enis.tn>
2023-10-13 13:02:46 +0100

    the comment

--- BillingWorkSpace/ConfigServer/src/main/java/com/example/demo/ConfigServerApplication.java
+++ BillingWorkSpace/ConfigServer/src/main/java/com/example/demo/ConfigServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
-
+ // add comment for the git
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
